0cdca52e73dcb513cd30eefe6fe7659f1e447fe8
amitparikh <35794224+amiparik@users.noreply.github.com>
2020-12-10 14:25:17 +0530

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -47,14 +47,14 @@ def main():
     for entry in myBudgetList:
             print (entry)
 
-    flg, ax = plt.subplots()
+    fig, ax = plt.subplots()
 
-    labels = ['Expenses','Overages','Budget'] 
+    labels = ['Expenses','Overages','Budget']
     values = [myBudgetList.sum_expenses,myBudgetList.sum_overages,myBudgetList.budget]
 
 
     ax.bar(labels, values, color=['green','red','blue'])
-    ax.set_title('Your total Expenses vs. total budget')
+    ax.set_title('Your total expenses vs. total budget')
     plt.show()
 
 if __name__ == "__main__":
